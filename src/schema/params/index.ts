cfcce77949f27cd415a917ba6cba892ae269bed7
Marcelo Brigato <marcelo.brigato_c@tdmsystems.com>
2026-05-01 15:57:40 -0300

    feat(schema): git param group

--- src/schema/params/index.ts
+++ src/schema/params/index.ts
@@ -1,8 +1,9 @@
 import type { ParamDef, ParamId } from '../types';
 import { IDENTITY_PARAMS } from './identity';
 import { WORKSPACE_PARAMS } from './workspace';
+import { GIT_PARAMS } from './git';
 
-export const PARAMS: ParamDef[] = [...IDENTITY_PARAMS, ...WORKSPACE_PARAMS];
+export const PARAMS: ParamDef[] = [...IDENTITY_PARAMS, ...WORKSPACE_PARAMS, ...GIT_PARAMS];
 
 export const PARAMS_BY_ID = Object.fromEntries(
   PARAMS.map((p) => [p.id, p]),
