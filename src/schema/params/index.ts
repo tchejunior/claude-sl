1acccd1d3e2fc1ecd838ddecdcf6330d8519bc09
Marcelo Brigato <marcelo.brigato_c@tdmsystems.com>
2026-05-01 15:57:59 -0300

    feat(schema): context param group

--- src/schema/params/index.ts
+++ src/schema/params/index.ts
@@ -2,8 +2,9 @@ import type { ParamDef, ParamId } from '../types';
 import { IDENTITY_PARAMS } from './identity';
 import { WORKSPACE_PARAMS } from './workspace';
 import { GIT_PARAMS } from './git';
+import { CONTEXT_PARAMS } from './context';
 
-export const PARAMS: ParamDef[] = [...IDENTITY_PARAMS, ...WORKSPACE_PARAMS, ...GIT_PARAMS];
+export const PARAMS: ParamDef[] = [...IDENTITY_PARAMS, ...WORKSPACE_PARAMS, ...GIT_PARAMS, ...CONTEXT_PARAMS];
 
 export const PARAMS_BY_ID = Object.fromEntries(
   PARAMS.map((p) => [p.id, p]),
